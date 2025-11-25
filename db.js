c8e672f22e6994f7d9677379c41bd315d936b408
David20020111 <davidykeila4@gmail.com>
2025-11-25 00:34:35 -0600

    Correccion en la conexion de la base de datos

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: process.env.DB_HOST || 'metro.proxy.rlwy.net', 
     port: process.env.DB_PORT || '35505',
     user: process.env.DB_USER || 'root',
