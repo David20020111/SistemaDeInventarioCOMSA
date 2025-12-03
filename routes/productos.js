530ee3bd5d3631c98783b5e43119e5d59a568c45
David20020111 <davidykeila4@gmail.com>
2025-12-03 13:41:26 -0600

    Correccion en filtros

--- routes/productos.js
+++ routes/productos.js
@@ -63,4 +63,44 @@ router.delete('/:id', (req, res) => {
   });
 });
 
+router.get("/", (req, res) => {
+  const { categoria, stock, nombre } = req.query;
+
+  let query = `
+    SELECT p.codigo, p.nombre, c.nombre AS categoria, 
+           p.stock_actual, p.stock_minimo, p.ubicacion
+    FROM productos p
+    JOIN categorias c ON p.id_categoria = c.id_categoria
+    WHERE 1=1
+  `;
+  const params = [];
+
+  if (categoria) {
+    query += " AND c.nombre LIKE ?";
+    params.push(`%${categoria}%`);
+  }
+
+  if (nombre) {
+    query += " AND p.nombre LIKE ?";
+    params.push(`%${nombre}%`);
+  }
+
+  if (stock === "bajo") {
+    query += " AND p.stock_actual < p.stock_minimo";
+  } else if (stock === "normal") {
+    query += " AND p.stock_actual >= p.stock_minimo";
+  }
+
+  query += " ORDER BY p.nombre ASC"
+
+  db.query(query, params, (err, results) => {
+    if (err) {
+      console.error("Error ejecutando reporte inventario: ", err);
+      return res.status(500).json({ error: "Error al generar reporte" });
+    }
+    res.json(results);
+  });
+});
+
+
 module.exports = router;
